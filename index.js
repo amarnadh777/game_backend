195680a83856007c09c5b4d8c8d04b7f540a39be
amarnadh777 <amarnadh6565@gmail.com>
2026-03-16 14:48:44 +0530

    cors issue fixd

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const connectDB = require("./config/db");
 require("dotenv").config();
 connectDB();    
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
